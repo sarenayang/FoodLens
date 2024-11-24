0f21ade0351226bc092849a53fd0654c13c59a7c
Maggie Li <limaggie2020@gmail.com>
2024-11-24 23:09:41 +0800

    removed bottom nav from login/signup pages

--- app/src/main/java/hk/hku/cs/foodlens/MainActivity.kt
+++ app/src/main/java/hk/hku/cs/foodlens/MainActivity.kt
@@ -1,9 +1,11 @@
 package hk.hku.cs.foodlens
 
 import android.os.Bundle
+import android.view.View
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
@@ -22,8 +24,8 @@ class MainActivity : AppCompatActivity() {
         val navView: BottomNavigationView = binding.navView
 
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
+
+        // Passing each menu ID as a set of Ids because each menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
             setOf(
                 R.id.navigation_restaurants, R.id.signon, R.id.navigation_profile
@@ -31,11 +33,24 @@ class MainActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+        // Set up a listener to hide/show BottomNavigationView based on fragment destination
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.signupFragment, R.id.loginFragment -> {
+                    // Hide the BottomNavigationView when on signup or login fragments
+                    navView.visibility = View.GONE
+                }
+                else -> {
+                    // Show the BottomNavigationView for other destinations
+                    navView.visibility = View.VISIBLE
+                }
+            }
+        }
     }
+
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
         return navController.navigateUp() || super.onSupportNavigateUp()
     }
-
-
 }
